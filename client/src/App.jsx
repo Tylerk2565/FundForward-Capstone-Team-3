cbdf52b2d7f7a4b003958e7a89cc261223023770
‘SkyPB’ <skyshanice01@gmail.com>
2025-03-11 09:56:08 -0400

    add pages and components folders in src folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
+      <h1 className="text-[#2E8B57]">Hello</h1>
     </>
   );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen"></div>
+    {/* Hero Section */}
+    <section className="bg-blue-600 text-white teext-center py-20 px-6">
+      <h1 className="text-4xl font-bold">Make a Difference with FundForward</h1>
+      <p className="mt-4 text-lg max-w-3xl mx-auto"> Yoour time and donations can create lasting changes in communities that need it most.</p>
+
+    </section>
+  )
+};
