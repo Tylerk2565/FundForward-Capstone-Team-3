51fc4b9ed072c91e8363d4199819b0b1e2a6d0b3
Tylerk2565 <ty25566@gmail.com>
2025-03-11 15:10:15 -0400

    Added and started Navbar + Footer, installed tailwind icon dependencies, created icons folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
-import "./App.css";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
     <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+        </Routes>
+        <Footer/> 
+      </Router>
     </>
   );
 }
