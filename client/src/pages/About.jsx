a8a0ed0a141fe2f3d77bbf37599c81a940479d13
Kevin Guzman <kevincastrog28@gmail.com>
2025-03-11 13:05:34 -0400

    Feat: Added Opportunites and Volunteer pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,7 +4,11 @@ const About = () => {
 
     return (
         <>
-            <h1>About Us</h1>
+            <div> 
+                <img src='https://i0.wp.com/www.theassemblync.com/wp-content/uploads/2022/09/TA_Skylines_01-1.jpg?fit=2400%2C1350&ssl=1' alt='Charlotte Skyline'></img>
+            </div>
         </>
     )
 }
+
+export default About
---
+++ client/src/pages/OpportunitieSearch.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const OpportunitiesSearch = () => {
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default OpportunitiesSearch
