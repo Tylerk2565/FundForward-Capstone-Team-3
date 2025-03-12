999be4ce00196318916e322001329b2af0766da4
Kevin Guzman <kevincastrog28@gmail.com>
2025-03-12 15:36:44 -0400

    fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import Fundraisers from "./pages/Fundraisers";
 const App = () => {
   return (
     <>
@@ -10,7 +10,9 @@ const App = () => {
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <div className="flex-grow">
-            <Routes>{/* <Route path="/" element={<HomePage />} /> */}</Routes>
+            <Routes>
+            <Route path="/fundraiser" element={<Fundraisers />} />
+              </Routes>
           </div>
           <Footer />
         </div>
