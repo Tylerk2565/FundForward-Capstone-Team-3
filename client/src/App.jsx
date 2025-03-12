0eb47056a1c61d416f3f6a8fbb3dacb246cd8912
Tylerk2565 <ty25566@gmail.com>
2025-03-12 11:19:24 -0400

    Finished Footer, Created more icons, Edited App.jsx to keep footer at bottom of page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Router>
-        <Navbar/>
-        <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */}
-        </Routes>
-        <Footer/> 
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-grow">
+            <Routes>{/* <Route path="/" element={<HomePage />} /> */}</Routes>
+          </div>
+          <Footer />
+        </div>
       </Router>
     </>
   );
-}
+};
 
 export default App;
