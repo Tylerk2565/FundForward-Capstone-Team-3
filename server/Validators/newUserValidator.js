0e9a6b0c2c5915282c6aabe62ef37fb287d0fea7
JerryC2005 <jerrycastroluz@gmail.com>
2025-03-12 18:12:32 -0400

    Feat: added register backend functionality and connected it to database

---
+++ server/Validators/newUserValidator.js
@@ -0,0 +1,29 @@
+import Joi from "joi"
+
+export const userSchema = Joi.object({
+    user: Joi.string()
+      .trim()                               // Removes leading/trailing spaces
+      .alphanum()                           // Ensures only letters/numbers
+      .min(3).required()
+      .messages({
+        'string.min': 'Username must be at least 3 characters long.',
+        'string.alphanum': 'Username can only contain letters and numbers.'
+      }),
+  
+    pwd: Joi.string()
+      .pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/)
+      .required()
+      .messages({
+        'string.pattern.base': 'Password must be strong (8+ chars, 1 uppercase, 1 lowercase, 1 number, 1 symbol).'
+      }),
+  
+    email: Joi.string()
+      .trim()
+      .email()                              // Ensures valid email
+      .lowercase()                          // Normalizes email
+      .required(),
+  
+    firstname: Joi.string().trim().replace(/[<>\/\\]/g, '').required(),
+    lastname: Joi.string().trim().replace(/[<>\/\\]/g, '').required(),
+    }
+)  
