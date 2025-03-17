9c5c86651e0aef35f2df660f11ab0763c9844789
Kevin Guzman <kevincastrog28@gmail.com>
2025-03-17 09:13:00 -0400

    updating branch

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           </NavLink>
           <NavLink
             className="relative px-3 py-2 rounded transition duration-300 ease-in-out hover:bg-gray-100"
-            to="/fundraiser"
+            to="/"
           >
             Fundraisers
           </NavLink>
